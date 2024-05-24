7b78b3578f7a6d08b968c8e5018953f8f75c27ab
Huzaifa <edusoftsystemsolutions@outlook.com>
2024-05-24 17:27:36 +0500

    Huzaifa 24-May-2024

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react"
-
+import Logo from "../assets/cheque-reliance-logo.png"
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="flex items-center justify-center w-full min-h-[90vh]">
+      <img
+        style={{ objectFit: "contain", height: "500px", width: "500px" }}
+        src={Logo}
+      ></img>
+    </div>
+  )
 }
 
 export default Home
